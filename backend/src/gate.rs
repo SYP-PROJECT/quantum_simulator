cfc12aedb7a2c52fc6fd5dbbb8e0c42961c1972c
MatyasZsombor <mzsombi@icloud.com>
2024-11-17 22:09:59 +0100

    Implemented the pauli y gate

--- backend/src/gate.rs
+++ backend/src/gate.rs
@@ -27,3 +27,26 @@ impl Gate for PauliX {
         self.matrix_form
     }
 }
+
+pub struct PauliY {
+    matrix_form: Matrix2<Complex<f64>>,
+}
+
+impl PauliY {
+    pub fn new() -> Self {
+        Self {
+            matrix_form: Matrix2::new(
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, -1.0),
+                Complex::new(0.0, 1.0),
+                Complex::new(0.0, 0.0),
+            ),
+        }
+    }
+}
+
+impl Gate for PauliY {
+    fn matrix_representation(&self) -> Matrix2<Complex<f64>> {
+        self.matrix_form
+    }
+}
