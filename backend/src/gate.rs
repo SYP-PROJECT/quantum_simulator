bbc69ea3e6128030597f154ba388f93bb863b8ef
MatyasZsombor <mzsombi@icloud.com>
2024-11-17 22:09:19 +0100

    Implemented the pauli x gate

---
+++ backend/src/gate.rs
@@ -0,0 +1,29 @@
+use nalgebra::Complex;
+use nalgebra::Matrix2;
+
+pub trait Gate {
+    fn matrix_representation(&self) -> Matrix2<Complex<f64>>;
+}
+
+pub struct PauliX {
+    matrix_form: Matrix2<Complex<f64>>,
+}
+
+impl PauliX {
+    pub fn new() -> Self {
+        Self {
+            matrix_form: Matrix2::new(
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+            ),
+        }
+    }
+}
+
+impl Gate for PauliX {
+    fn matrix_representation(&self) -> Matrix2<Complex<f64>> {
+        self.matrix_form
+    }
+}
