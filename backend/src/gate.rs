7f0b803fbf130d262610c46da8c4952150ef1047
MatyasZsombor <mzsombi@icloud.com>
2024-11-17 22:22:16 +0100

    Implemented the hadamard gate

--- backend/src/gate.rs
+++ backend/src/gate.rs
@@ -5,6 +5,29 @@ pub trait Gate {
     fn matrix_representation(&self) -> Matrix2<Complex<f64>>;
 }
 
+pub struct Identity {
+    matrix_form: Matrix2<Complex<f64>>,
+}
+
+impl Identity {
+    pub fn new() -> Self {
+        Self {
+            matrix_form: Matrix2::new(
+                Complex::new(1.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(0.0, 0.0),
+                Complex::new(1.0, 0.0),
+            ),
+        }
+    }
+}
+
+impl Gate for Identity {
+    fn matrix_representation(&self) -> Matrix2<Complex<f64>> {
+        self.matrix_form
+    }
+}
+
 pub struct PauliX {
     matrix_form: Matrix2<Complex<f64>>,
 }
@@ -69,6 +92,33 @@ impl PauliZ {
 }
 
 impl Gate for PauliZ {
+    fn matrix_representation(&self) -> Matrix2<Complex<f64>> {
+        self.matrix_form
+    }
+}
+
+pub struct Hadamard {
+    matrix_form: Matrix2<Complex<f64>>,
+}
+
+impl Hadamard {
+    pub fn new() -> Self {
+        let mut matrix = Matrix2::new(
+            Complex::new(1.0, 0.0),
+            Complex::new(1.0, 0.0),
+            Complex::new(1.0, 0.0),
+            Complex::new(-1.0, 0.0),
+        );
+
+        matrix = matrix * Complex::new(1.0 / (2.0_f64).sqrt(), 0.0);
+
+        Self {
+            matrix_form: matrix,
+        }
+    }
+}
+
+impl Gate for Hadamard {
     fn matrix_representation(&self) -> Matrix2<Complex<f64>> {
         self.matrix_form
     }
