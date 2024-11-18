f7559911f2bebc2a649a0336878f6cb0677df4e4
MatyasZsombor <mzsombi@icloud.com>
2024-11-18 14:52:54 +0100

    Defined the lexing rules

--- frontend/src/lexer.ts
+++ frontend/src/lexer.ts
@@ -14,3 +14,27 @@ enum TokenType {
   IMAGINARY_UNIT = "IMAGINARY_UNIT",
   WHITESPACE = "WHITESPACE"
 }
+
+interface Token {
+  type: TokenType;
+  value: string;
+}
+
+class Lexer {
+  private rules: { type: TokenType; regex: RegExp }[] = [
+    { type: TokenType.CREATE, regex: /^create/ },
+    { type: TokenType.QUBIT, regex: /^qubit/ },
+    { type: TokenType.CONNECT, regex: /^connect/ },
+    { type: TokenType.IDENTIFIER, regex: /^[a-zA-Z_][a-zA-Z0-9_]*/ },
+    { type: TokenType.EQUALS, regex: /^=/ },
+    { type: TokenType.COMMA, regex: /^,/ },
+    { type: TokenType.SEMICOLON, regex: /^;/ },
+    { type: TokenType.LBRACKET, regex: /^\[/ },
+    { type: TokenType.RBRACKET, regex: /^\]/ },
+    { type: TokenType.PLUS, regex: /^\+/ },
+    { type: TokenType.MINUS, regex: /^-/ },
+    { type: TokenType.NUMBER, regex: /^[0-9]+(\.[0-9]+)?/ },
+    { type: TokenType.IMAGINARY_UNIT, regex: /^i/ },
+    { type: TokenType.WHITESPACE, regex: /^\s+/ }
+  ];
+}
