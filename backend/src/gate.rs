7e40e67c09963b6ccce3fa327335a3ec115fcddb
MatyasZsombor <mzsombi@icloud.com>
2024-11-18 11:52:39 +0100

    Addded unit test to identity gate

--- backend/src/gate.rs
+++ backend/src/gate.rs
@@ -110,7 +110,7 @@ impl Hadamard {
             Complex::new(-1.0, 0.0),
         );
 
-        matrix = matrix * Complex::new(1.0 / (2.0_f64).sqrt(), 0.0);
+        matrix *= Complex::new(1.0 / (2.0_f64).sqrt(), 0.0);
 
         Self {
             matrix_form: matrix,
@@ -121,5 +121,32 @@ impl Hadamard {
 impl Gate for Hadamard {
     fn matrix_representation(&self) -> Matrix2<Complex<f64>> {
         self.matrix_form
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::qubit::Qubit;
+
+    use super::*;
+
+    #[test]
+    fn test_identity_gate() {
+        let identity = Identity::new();
+
+        let mut qubit = Qubit::basis0();
+        qubit.apply_gate(&identity);
+        assert_eq!(qubit.state(), Qubit::basis0().state());
+
+        qubit = Qubit::basis1();
+        qubit.apply_gate(&identity);
+        assert_eq!(qubit.state(), Qubit::basis1().state());
+
+        let mut qubit = Qubit::new_from_amplitudes(1.0, 1.0, 1.0, 1.0);
+        qubit.apply_gate(&identity);
+        assert_eq!(
+            qubit.state(),
+            Qubit::new_from_amplitudes(1.0, 1.0, 1.0, 1.0).state()
+        );
     }
 }
