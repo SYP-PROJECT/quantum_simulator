df81c6ffabddb45f6efa25fa6a44da513874556f
MatyasZsombor <mzsombi@icloud.com>
2024-11-25 13:18:49 +0100

    Added extra unit tests for lexer
    #29

--- frontend/src/input_handler/lexer.spec.ts
+++ frontend/src/input_handler/lexer.spec.ts
@@ -1,10 +1,9 @@
 import { Lexer, TokenType } from "./lexer"
 
-
 describe("Lexer", () => {
-  test("tokenizes a simple create qubit statement", () => {
-    const input = "create qubit q1 = [1 + 2i, 2 - 3i];";
-    const tokens = Lexer.tokenize(input);
+  test("test a simple create qubit statement", () => {
+    const lexer = new Lexer("create qubit q1 = [1 + 2i, 2 - 3i];");
+    const tokens = lexer.tokenize();
 
     expect(tokens).toEqual([
       { type: TokenType.CREATE, value: "create" },
@@ -24,5 +23,61 @@ describe("Lexer", () => {
       { type: TokenType.RBRACKET, value: "]" },
       { type: TokenType.SEMICOLON, value: ";" },
     ]);
+  });
+
+  test("test numbers with floating points", () => {
+    const lexer = new Lexer("create qubit q2 = [1.5 + 2.3i, -3.1 - 4.0i];");
+    const tokens = lexer.tokenize();
+
+    expect(tokens).toEqual([
+      { type: TokenType.CREATE, value: "create" },
+      { type: TokenType.QUBIT, value: "qubit" },
+      { type: TokenType.IDENTIFIER, value: "q2" },
+      { type: TokenType.EQUALS, value: "=" },
+      { type: TokenType.LBRACKET, value: "[" },
+      { type: TokenType.NUMBER, value: "1.5" },
+      { type: TokenType.PLUS, value: "+" },
+      { type: TokenType.NUMBER, value: "2.3" },
+      { type: TokenType.IMAGINARY_UNIT, value: "i" },
+      { type: TokenType.COMMA, value: "," },
+      { type: TokenType.MINUS, value: "-" },
+      { type: TokenType.NUMBER, value: "3.1" },
+      { type: TokenType.MINUS, value: "-" },
+      { type: TokenType.NUMBER, value: "4.0" },
+      { type: TokenType.IMAGINARY_UNIT, value: "i" },
+      { type: TokenType.RBRACKET, value: "]" },
+      { type: TokenType.SEMICOLON, value: ";" },
+    ]);
+  });
+
+  test("test identifier with i", () => {
+    const lexer = new Lexer("create qubit i = [1 + 2i, -3 - 4i];");
+    const tokens = lexer.tokenize();
+
+    expect(tokens).toEqual([
+      { type: TokenType.CREATE, value: "create" },
+      { type: TokenType.QUBIT, value: "qubit" },
+      { type: TokenType.IDENTIFIER, value: "i" },
+      { type: TokenType.EQUALS, value: "=" },
+      { type: TokenType.LBRACKET, value: "[" },
+      { type: TokenType.NUMBER, value: "1" },
+      { type: TokenType.PLUS, value: "+" },
+      { type: TokenType.NUMBER, value: "2" },
+      { type: TokenType.IMAGINARY_UNIT, value: "i" },
+      { type: TokenType.COMMA, value: "," },
+      { type: TokenType.MINUS, value: "-" },
+      { type: TokenType.NUMBER, value: "3" },
+      { type: TokenType.MINUS, value: "-" },
+      { type: TokenType.NUMBER, value: "4" },
+      { type: TokenType.IMAGINARY_UNIT, value: "i" },
+      { type: TokenType.RBRACKET, value: "]" },
+      { type: TokenType.SEMICOLON, value: ";" },
+
+    ]);
+  });
+
+  test("test for unexpected tokens", () => {
+    const lexer = new Lexer("create unknown@");
+    expect(() => lexer.tokenize()).toThrowError("Unexpected token: @");
   });
 })
