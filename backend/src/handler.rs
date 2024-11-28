570f7974a0e91f914b85e4edfe151baff2f6c8bf
MatyasZsombor <mzsombi@icloud.com>
2024-11-28 21:52:15 +0100

    Added handler function
    #41

--- backend/src/handler.rs
+++ backend/src/handler.rs
@@ -1,1 +1,10 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
+
+use crate::models::ProgramNode;
+
+pub async fn simulation_handler(
+    Json(body): Json<ProgramNode>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    println!("{:?}", body);
+    Ok(StatusCode::NO_CONTENT)
+}
