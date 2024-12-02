d53b8f4d73bd349f3b258f9d63c48097e1df6f05
Roland Fornvald <r.fornvald@HTBLALeonding.onmicrosoft.com>
2024-12-02 08:08:03 +0100

    Finished basic UI #34

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         <div style={{ flex: 1 }}>
           <Editor
               height="100vh"
+              width="100%"
               defaultLanguage="javascript"
               defaultValue="// Start coding here..."
               theme="vs-dark"
@@ -23,35 +24,34 @@ export default function Home() {
         </div>
 
         {}
-        <div style={{ width: '300px', padding: '10px', background: '#333', color: '#fff' }}>
-          <button
-              onClick={handleButtonClick}
-              style={{
-                marginBottom: '20px',
-                padding: '10px',
-                background: '#444',
-                border: 'none',
-                color: '#fff',
-                cursor: 'pointer',
-              }}
-          >
-            Run Code
-          </button>
-
-          {}
-          <div
-              style={{
-                background: '#222',
-                padding: '10px',
-                height: 'calc(100vh - 50px)', // Adjust the height based on button size
-                overflowY: 'auto',
-                whiteSpace: 'pre-wrap', // Keep the formatting of the output
-                borderRadius: '5px',
-              }}
-          >
-            {output}
+          <div style={{width: '40%', padding: '10px', background: '#333', color: '#fff'}}>
+              {}
+              <div
+                  style={{
+                      background: '#222',
+                      padding: '10px',
+                      height: 'calc(100vh - 50px)', // Adjust the height based on button size
+                      overflowY: 'auto',
+                      whiteSpace: 'pre-wrap', // Keep the formatting of the output
+                      borderRadius: '5px',
+                  }}
+              >
+                  {output}
+              </div>
+              <button
+                  onClick={handleButtonClick}
+                  style={{
+                      marginBottom: '20px',
+                      padding: '10px',
+                      background: '#444',
+                      border: 'none',
+                      color: '#fff',
+                      cursor: 'pointer',
+                  }}
+              >
+                  Run Code
+              </button>
           </div>
-        </div>
       </div>
   );
 }
