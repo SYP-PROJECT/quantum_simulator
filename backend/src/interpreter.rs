99f57e86b58929fbd42710a16ea00f95fe007614
MatyasZsombor <mzsombi@icloud.com>
2024-12-02 20:11:07 +0100

    Added interpretation to create statements
    #52

--- backend/src/interpreter.rs
+++ backend/src/interpreter.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use crate::{
-    models::{ProgramNode, StatementNode},
+    models::{ComplexNumberNode, CreateStatementNode, ProgramNode, StatementNode},
     qubit::Qubit,
 };
 
@@ -26,6 +26,50 @@ fn interpret_statement(
     statement: StatementNode,
     variables: &mut HashMap<String, Qubit>,
 ) -> Option<String> {
-    None
+    match statement {
+        StatementNode::Create(create_statement) => {
+            let identifier = create_statement.identifier;
+            if variables.contains_key(&identifier){
+                Some(format!("Identifier {} was already declared", identifier))
+            }
+            else
+            {
+                let complex_array = create_statement.complex_array;
+                let complex1 = &complex_array.values[0];
+                let complex2 = &complex_array.values[1];
+
+                let qubit = Qubit::new_from_amplitudes(
+                    get_real_part(complex1), 
+                    get_imaginary_part(complex1),
+                    get_real_part(complex2),
+                    get_imaginary_part(complex2)
+                );
+                
+                variables.insert(identifier, qubit);
+                None
+            }
+        },
+        StatementNode::Connect(_) => {
+          None
+        },
+        StatementNode::Measure(measure_statement) => {
+            None        },
+        StatementNode::Display(_) => {
+            None
+        }
+    }
 }
 
+fn get_real_part(complex_node: &ComplexNumberNode) -> f64 {
+    if complex_node.real_part.is_some(){
+      return complex_node.real_part.as_ref().unwrap().value;
+    }
+    0.0
+}
+
+fn get_imaginary_part(complex_node: &ComplexNumberNode) -> f64 {
+    if complex_node.imaginary_part.is_some(){
+      return complex_node.imaginary_part.as_ref().unwrap().value;
+    }
+    0.0
+}
