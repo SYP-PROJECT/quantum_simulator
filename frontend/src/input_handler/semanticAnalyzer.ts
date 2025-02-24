11f2214d9f011c29ccb585474b143b11f5d1bf9c
MatyasZsombor <mzsombi@icloud.com>
2025-02-24 11:41:54 +0100

    Move semantic analyzing to the frontend
    #77

---
+++ frontend/src/input_handler/semanticAnalyzer.ts
@@ -0,0 +1,76 @@
+import { NodeType, ProgramNode, StatementNode, CreateStatementNode, ApplyStatementNode, MeasureStatementNode, DisplayStatementNode } from "./ast";
+
+export class SemanticAnalyzer {
+  readonly variables: string[] = [];
+  readonly gates: string[] = [];
+  public readonly Errors: string[] = [];
+
+  SemanticAnalyzer() {
+    this.initializeGateMap();
+  }
+
+  public analyze(programNode: ProgramNode) {
+    for (const statement of programNode.statements) {
+      this.analyzeStatement(statement);
+    }
+  }
+
+  analyzeStatement(statement: StatementNode) {
+    switch (statement.type) {
+      case NodeType.CreateStatement: {
+        let identifier = (statement as CreateStatementNode).identifier;
+
+        if (this.variables.includes(identifier)) {
+          this.Errors.push(`Identifier '${identifier}' was already declared`);
+        }
+        else {
+          this.variables.push(identifier);
+        }
+        break;
+      }
+
+      case NodeType.ApplyStatement: {
+        let applyStatement = (statement as ApplyStatementNode);
+
+        let identifier1 = applyStatement.identifier1;
+        let identifier2 = applyStatement.identifier2;
+
+        if (!this.variables.includes(identifier1)) {
+          this.Errors.push(`Cannot resolve symbol '${identifier1}'`);
+        }
+
+        if (!this.variables.includes(identifier2)) {
+          this.Errors.push(`Cannot resolve symbol '${identifier2}'`);
+        }
+        break;
+      }
+
+      case NodeType.MeasureStatement: {
+        let identifier = (statement as MeasureStatementNode).identifier;
+
+        if (!this.variables.includes(identifier)) {
+          this.Errors.push(`Cannot resolve symbol '${identifier}'`);
+        }
+        break;
+      }
+
+      case NodeType.DisplayStatement: {
+        let identifier = (statement as DisplayStatementNode).identifier;
+
+        if (!this.variables.includes(identifier)) {
+          this.Errors.push(`Cannot resolve symbol '${identifier}'`);
+        }
+        break;
+      }
+
+    }
+  }
+
+  initializeGateMap() {
+    this.gates.push("identity");
+    this.gates.push("pauliX");
+    this.gates.push("pauliY");
+    this.gates.push("pauliZ");
+    this.gates.push("hadamard");
+  }
+}
