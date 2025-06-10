8eba0fbecdfa531f76cd7aa04572beb58b755aca
BintingerNico <n.bintinger@students.htl-leonding.ac.at>
2025-06-10 09:40:24 +0200

    finished basic analytics

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,12 +1,14 @@
-declare global {
-    interface Window {
-        gtag: (...args: Gtag.GtagCommands[]) => void;
-    }
-}
-
 declare namespace Gtag {
     type GtagCommands =
         | ['js', Date]
         | ['config', string, Record<string, unknown>?]
         | ['event', string, Record<string, unknown>?];
 }
+
+declare global {
+    interface Window {
+        gtag: (...args: unknown[]) => void;
+    }
+}
+
+export {};
